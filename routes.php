8be35a1c95d57dc04b58d299f86b1e641b0b7708
Ben Rogers <nodren@gmail.com>
2012-11-04 16:57:32 -0800

    updating route to work with root url

--- routes.php
+++ routes.php
@@ -6,3 +6,9 @@ Route::any('(:bundle)/(:any)?', function ($page = '')
 {
 	return Controller::call('cello::pages@index', array($page));
 });
+Route::any('/', function() {
+	//only take over the root uri if cello is set for that
+	if (Bundle::handles('/') == 'cello') {
+		return Controller::call('cello::pages@index', array(''));	
+	}
+});
